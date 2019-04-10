4925a98d632a1e143360e6b82a30ed483398beee
Jaewoo Park <jaewoopark91@gmail.com>
2019-04-09 22:37:19 -0700

    [DEND-1] Initial postgres link with docker
    Adds docker compose yaml to launch postgres in a container, and
    confirmed connection with docker exec.

---
+++ README.md
@@ -0,0 +1,13 @@
+# Relational Data Modeling Project
+WIP: This project aims to model songs and log data into postgresql. Based on the data mart schema created. 
+
+## Setting up postgresql locally with Docker
+
+    docker-compose up -d
+
+This should bring up a docker container which exposes its postgresql port to 54320 locally.
+
+## Installing required Python packages
+Install the required packages with pipenv
+
+    pipenv install
--- create_tables.py
+++ create_tables.py
@@ -4,7 +4,12 @@ from sql_queries import create_table_queries, drop_table_queries
 
 def create_database():
     # connect to default database
-    conn = psycopg2.connect("host=127.0.0.1 dbname=studentdb user=student password=student")
+    conn = psycopg2.connect(
+        host='localhost',
+        port=54320,
+        dbname='my_database',
+        user='posgres'
+    )
     conn.set_session(autocommit=True)
     cur = conn.cursor()
     
